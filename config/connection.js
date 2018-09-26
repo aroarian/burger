ae7bb67ce17c19e03af7ff361f6c48703485a3ce
aroarian <aroarian0@gmail.com>
2018-09-26 18:11:16 -0400

    sfsdf

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+  if (err) throw err
   console.log("connected as id " + connection.threadId);
 });
 
