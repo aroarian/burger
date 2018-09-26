5f0a2e71768d933d5fff1aa235ce796952db2ca3
aroarian <aroarian0@gmail.com>
2018-09-26 18:25:13 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "7654321z",
   database: "burgers_db"
-});
+ });
+};
 
 connection.connect(function(err) {
   if (err) throw err
